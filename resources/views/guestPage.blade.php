978b5ca0eb404b74047f4715db7023e2be2e0825
pietrocavallin <69079102+pietrocavallin@users.noreply.github.com>
2023-02-09 12:05:04 +0100

    fix

--- resources/views/guestPage.blade.php
+++ resources/views/guestPage.blade.php
@@ -1,7 +1,10 @@
 @extends('layout')
 
 @section('body')
-    <h1 class="text-7xl font-bold text-violet-700">
-        Sei in attesa che il cordinatore ti dia un ruolo
-    </h1>
+    <div class="flex w-screen h-screen justify-center items-center">
+        <img src="{{ asset('/images/2.png')}}" alt="">
+        <h1 class="text-7xl font-bold text-violet-700">
+            Sei in attesa che il coordinatore ti dia un ruolo
+        </h1>
+    </div>
 @endsection
